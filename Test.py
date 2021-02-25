e7353c2b42a321ff0c41e28161a914590c3f8f24
zgy <515783116@qq.com>
2021-02-25 20:45:49 +0800

    Update README.md

--- Test.py
+++ Test.py
@@ -1,21 +1,21 @@
-s ='r,f,s,h,s,h,h,s,f,f,f'
-s2 = s.split(",")
-print(len(s2))
-r = 0
-f = 0
-s = 0
-h = 0
-for i in s2:
-    if i=='r':
-        r+=1
-    if i=='f':
-        f+=1
-    if i=='s':
-        s+=1
-    if i=='h':
-        h+=1
+s ='rfshshhsfff'
+# s2 = s.split(",")
+# print(len(s2))
+r = s.count('r')
+f = s.count('f')
+s2 = s.count('s')
+h = s.count('h')
+# for i in s2:
+#     if i=='r':
+#         r+=1
+#     if i=='f':
+#         f+=1
+#     if i=='s':
+#         s+=1
+#     if i=='h':
+#         h+=1
 
 print("r的个数：",r)
 print("f的个数：",f)
-print("s的个数：",s)
+print("s的个数：",s2)
 print("h的个数：",h)
